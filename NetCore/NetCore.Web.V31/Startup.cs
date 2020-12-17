d7944a1184773240284368734eb6dec6e4303ab4
jsootv <jsootv@gmail.com>
2020-12-17 19:19:41 +0900

    Startup.cs, appsettings.json, Program.cs에 강의 내용을 적용하였습니다.

--- NetCore/NetCore.Web.V31/Startup.cs
+++ NetCore/NetCore.Web.V31/Startup.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NetCore.Services.Data;
+using NetCore.Services.Interfaces;
+using NetCore.Services.Svcs;
+using NetCore.Utilities.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,8 +30,58 @@ namespace NetCore.Web.V31
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #region 강의내용A
+            Common.SetDataProtection(services, @"D:\DataProtector\", "NetCore", Enums.CryptoType.CngCbc);
+
+            //껍데기                         내용물
+            //IUser 인터페이스가 UserService 클래스를 받기 위해 services에 등록해야 함.
+            services.AddScoped<DBFirstDbInitializer>();
+            services.AddScoped<IUser, UserService>();
+            services.AddScoped<IPasswordHasher, PasswordHasher>();
+
+            services.AddHttpContextAccessor();
+
+            //DB접속정보, Migrations 프로젝트 지정
+            //services.AddDbContext<CodeFirstDbContext>(options =>
+            //            options.UseSqlServer(connectionString: Configuration.GetConnectionString(name: "DefaultConnection"),
+            //                                 sqlServerOptionsAction: mig => mig.MigrationsAssembly(assemblyName: "NetCore.Migrations")));
+
+            //DB접속정보만
+            services.AddDbContext<DBFirstDbContext>(options =>
+                        options.UseSqlServer(connectionString: Configuration.GetConnectionString(name: "DBFirstDBConnection")));
+
+            //Logging
+            services.AddLogging(builder =>
+            {
+                builder.AddConfiguration(Configuration.GetSection(key: "Logging"));
+                builder.AddConsole();
+                builder.AddDebug();
+            });
+            #endregion
+
             // .Net Core 2.1의 AddMvc()에서 다음과 같이 메서드명이 변경됨. 
             services.AddControllersWithViews();
+
+            #region 강의내용B
+            //신원보증과 승인권한
+            services.AddAuthentication(defaultScheme: CookieAuthenticationDefaults.AuthenticationScheme)
+                    .AddCookie(options =>
+                    {
+                        options.AccessDeniedPath = "/Membership/Forbidden";
+                        options.LoginPath = "/Membership/Login";
+                    });
+
+            services.AddAuthorization();
+
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.Cookie.Name = ".NetCore.Session";
+                //세션 제한시간
+                options.IdleTimeout = TimeSpan.FromMinutes(30); //기본값은 20분
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +105,16 @@ namespace NetCore.Web.V31
 
             // 승인권한을 사용하기 위해 추가됨.
             app.UseAuthorization();
+            
+            #region 강의내용
+            //신원보증만
+            app.UseAuthentication();
+
+            //세션 지정
+            //System.InvalidOperationException:
+            //'Session has not been configured for this application or request.'
+            app.UseSession();
+            #endregion
 
             // .Net Core 2.1의 UseMvc()에서 다음과 같이 메서드명이 변경됨. 
             app.UseEndpoints(endpoints =>
