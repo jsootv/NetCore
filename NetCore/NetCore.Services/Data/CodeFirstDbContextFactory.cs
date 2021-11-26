042fff632f24423dd07c68e912bb2f586ba4e6a6
Jeong-soo, Kim <programmingsquare@gmail.com>
2021-11-26 20:51:21 +0900

    마이그레이션을 위한 DbContextFactory 작업

---
+++ NetCore/NetCore.Services/Data/CodeFirstDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using NetCore.Services.Config;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCore.Services.Data
+{
+    public class CodeFirstDbContextFactory
+        : IDesignTimeDbContextFactory<CodeFirstDbContext>
+    {
+        /// <summary>
+        /// appsettings.json 경로
+        /// </summary>
+        private const string _configPath = @"D:\Repository\jsootv\NetCore\NetCore\NetCore.Web.V5\appsettings.json";
+
+        public CodeFirstDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CodeFirstDbContext>();
+            // NetCore.Web.V5 프로젝트의 appsettings.json의 DB연결문자열을 가져와야 한다.
+            optionsBuilder.UseSqlServer(new DbConnector(_configPath).GetConnectionString());
+
+            return new CodeFirstDbContext(optionsBuilder.Options);
+        }
+    }
+}
